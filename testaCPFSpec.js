f38d862a28967cb9b3a2b7f8ba0712246cfe02ef
TexTex0 <106408687+TexTex0@users.noreply.github.com>
2023-01-31 11:53:48 -0300

    Update testaCPFSpec.js

--- testaCPFSpec.js
+++ testaCPFSpec.js
@@ -1,9 +1,10 @@
 
-describe("Testando cpf", function(){
-  
-  it("VERDADEIRO", function(){
-  
-    expect(TestaCPF('66278235005')).toBeTruthy();
+describe("Testando cpf", function(){describe('TestaCPF', () => {
+  it('Deve retornar true para um CPF válido', () => {
+    expect(TestaCPF('66278235005')).toBe(true);
+  });
+  it('Deve retornar false para um CPF inválido', () => {
+    expect(TestaCPF('11110001111')).toBe(false);
   });
 });
 
